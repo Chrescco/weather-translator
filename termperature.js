987c76178ff6987d1aafb879c7d627d9abbd66d0
Chrescco <98253438+Chrescco@users.noreply.github.com>
2022-02-09 10:10:40 +0530

    Add files via upload

---
+++ termperature.js
@@ -0,0 +1,5 @@
+const kelvin = 0;     //assignin a variable to kelvin
+const celsius = kelvin - 273;     //assigning celsius the result of 293 - 273 
+let fahrenheit = 68;   //assigning a value to fahrenheit Celsius * (9/5) + 32
+fahrenheit = Math.floor(fahrenheit);   //rounding of the number to a 'whole' number
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
